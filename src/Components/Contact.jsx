8f7150d335238ecf04226f3a8e3d145d6f363bdd
salamus <salamus@yandex.com>
2024-08-15 16:03:04 +0300

    change mao

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,28 +6,32 @@ export default function Contact() {
             <h1 className="contact-title">Контакты</h1>
             <div className="contact-content">
                 <div className="map-container">
-                    <div style={{ position: "relative", overflow: "hidden" }}>
-                        <a 
-                            href="https://yandex.ru/maps/193/voronezh/?utm_medium=mapframe&utm_source=maps" 
-                            style={{ color: "#eee", fontSize: "12px", position: "absolute", top: "0px" }}
-                        >
-                            Воронеж
-                        </a>
-                        <a 
-                            href="https://yandex.ru/maps/193/voronezh/house/rizhskaya_ulitsa_16/Z0AYdA5gSEYAQFtrfXp3c3tiYA==/?ll=39.290136%2C51.662714&utm_medium=mapframe&utm_source=maps&z=17" 
-                            style={{ color: "#eee", fontSize: "12px", position: "absolute", top: "14px" }}
-                        >
-                            Рижская улица, 16 на карте Воронежа — Яндекс Карты
-                        </a>
-                        <iframe 
-                            src="https://yandex.ru/map-widget/v1/?ll=39.290136%2C51.662714&mode=whatshere&whatshere%5Bpoint%5D=39.290137%2C51.662714&whatshere%5Bzoom%5D=17&z=17" 
-                            width="560" 
-                            height="400" 
-                            frameBorder="1" 
-                            allowFullScreen 
-                            style={{ position: "relative" }}
-                        ></iframe>
-                    </div>
+                <div style={{ position: 'relative', overflow: 'hidden' }}>
+      <a 
+        href="https://yandex.ru/maps/193/voronezh/?utm_medium=mapframe&utm_source=maps" 
+        style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '0px' }}
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        Воронеж
+      </a>
+      <a 
+        href="https://yandex.ru/maps/193/voronezh/house/olimpiyskiy_bulvar_6/Z0AYdw5oT0UAQFtrfXtwc3RjZw==/?ll=39.198607%2C51.712803&utm_medium=mapframe&utm_source=maps&z=17" 
+        style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '14px' }}
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        Олимпийский бульвар, 6 на карте Воронежа — Яндекс Карты
+      </a>
+      <iframe 
+        src="https://yandex.ru/map-widget/v1/?ll=39.198607%2C51.712803&mode=whatshere&whatshere%5Bpoint%5D=39.198607%2C51.712803&whatshere%5Bzoom%5D=17&z=17" 
+        width="560" 
+        height="400" 
+        frameBorder="1" 
+        allowFullScreen
+        style={{ position: 'relative' }}
+      ></iframe>
+    </div>
                 </div>
                 <div className="contact-info">
                     <p>
